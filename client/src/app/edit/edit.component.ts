a0431c127ab4c70f196112919e6ca03ae22535d6
frunjik <frunjik@gmail.com>
2018-10-13 14:38:13 +0200

    panels

--- client/src/app/edit/edit.component.ts
+++ client/src/app/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,29 +6,62 @@ import { Router } from '@angular/router';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent {
 
-  filenames = [
-      './README.md',
-      'server/controllers/files.js',
-      'client/package.json',
-      'client/src/index.html',
-      'client/src/app/app.module.ts',
-      'client/src/app/app.component.ts',
-      'client/src/app/app.component.html',
-      'client/src/app/app.component.css',
-      'client/src/app/edit/edit.component.ts',
-      'client/src/app/edit/edit.component.html',
-      'client/src/app/edit/edit.component.css',
-      'client/src/app/editor/editor.component.ts',
-      'client/src/app/editor/editor.component.html',
-      'client/src/app/editor/editor.component.css',
-      'client/src/app/home/home.component.ts',
-      'client/src/app/home/home.component.html',
-      'client/src/app/home/home.component.css',
-      'client/src/app/pagenotfound/pagenotfound.component.ts',
-      'client/src/app/pagenotfound/pagenotfound.component.html',
-      'client/src/app/pagenotfound/pagenotfound.component.css',
+  panels = [
+      { 
+        title: 'app',
+        filenames: [
+            'client/src/app/app.component.ts',
+            'client/src/app/app.component.html',
+            'client/src/app/app.component.css',
+            'client/src/app/home/home.component.ts',
+            'client/src/app/home/home.component.html',
+            'client/src/app/home/home.component.css',
+        ]
+      },
+      { 
+        title: 'edit',
+        filenames: [
+            'client/src/app/edit/edit.component.ts',
+            'client/src/app/edit/edit.component.html',
+            'client/src/app/edit/edit.component.css',
+            'client/src/app/editor/editor.component.ts',
+            'client/src/app/editor/editor.component.html',
+            'client/src/app/editor/editor.component.css',
+        ]
+      },
+      { 
+        title: 'client',
+        filenames: [
+            'client/package.json',
+            'client/src/index.html',
+            'client/src/app/fs.service.ts',
+            'client/src/app/app.module.ts',
+            'client/src/app/app.component.ts',
+            'client/src/app/app.component.html',
+            'client/src/app/app.component.css',
+            'client/src/app/edit/edit.component.ts',
+            'client/src/app/edit/edit.component.html',
+            'client/src/app/edit/edit.component.css',
+            'client/src/app/editor/editor.component.ts',
+            'client/src/app/editor/editor.component.html',
+            'client/src/app/editor/editor.component.css',
+            'client/src/app/home/home.component.ts',
+            'client/src/app/home/home.component.html',
+            'client/src/app/home/home.component.css',
+            'client/src/app/pagenotfound/pagenotfound.component.ts',
+            'client/src/app/pagenotfound/pagenotfound.component.html',
+            'client/src/app/pagenotfound/pagenotfound.component.css',
+        ]
+      },
+      { 
+        title: 'server',
+        filenames: [
+            './README.md',
+            'server/controllers/files.js',
+        ]
+      },
   ];
 
   constructor(private router: Router) {
