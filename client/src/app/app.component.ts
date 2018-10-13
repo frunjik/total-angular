4e2618ab025395d934ba3975672d503762a5606e
frunjik <frunjik@gmail.com>
2018-10-13 10:26:41 +0200

    file list

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showFiller = false;
   title = 'app';
+  links = [
+      'server/controllers/files.js',
+      'client/package.json',
+      'client/src/app/app.module.ts',
+      'client/src/app/app.component.ts',
+      'client/src/app/app.component.html',
+      'client/src/app/app.component.css',
+      'client/src/app/editor/editor.component.ts',
+      'client/src/app/editor/editor.component.html',
+      'client/src/app/editor/editor.component.css',
+      'client/src/app/pagenotfound/pagenotfound.component.ts',
+      'client/src/app/pagenotfound/pagenotfound.component.html',
+      'client/src/app/pagenotfound/pagenotfound.component.css',
+  ];
+
+    constructor(private router: Router) {
+    }  
+
+    gotoLink(l) {
+        //this.router.navigate('/edit', {name: l});
+        this.router.navigateByUrl("/edit?name="+l);
+        this.panelOpenState = false;
+    }
 }
