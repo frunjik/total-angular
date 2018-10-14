a65a1013b62f1bce480412bb30e19fc0505a5684
frunjik <frunjik@gmail.com>
2018-10-14 13:43:59 +0200

    start stop

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { start, stop } from '../js/main';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  private context;
+
+  /** Template reference to the canvas element */
+  @ViewChild('scene') canvasElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.context = (this.canvasElement.nativeElement as HTMLCanvasElement).getContext('webgl2');
+    start(this.context);
+  }
+
+  ngOnDestroy() {
+    stop(this.context);
+    this.context = undefined;
+  }
 }
