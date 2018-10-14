b618db3241bdb317b7fd459301a22f36aaf28106
frunjik <frunjik@gmail.com>
2018-10-14 13:41:42 +0200

    skeleton

---
+++ client/src/app/js/game.ts
@@ -0,0 +1,25 @@
+"use strict";
+function create() {
+}
+function resize() {
+}
+function start() {
+}
+function update(deltaTime) {
+}
+function render() {
+}
+function stop() {
+}
+function destroy() {
+}
+function on(event, handler) {
+}
+
+let game = create();
+game.start();
+game.on('resize', resize);
+game.on('update', update);
+game.on('render', render);
+game.stop();
+game.destroy();
