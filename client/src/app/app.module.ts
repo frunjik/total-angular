9ec036ab0456d0218c911d0a058785558cc5a61f
frunjik <frunjik@gmail.com>
2018-10-15 22:07:58 +0200

    material module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,97 +1,23 @@
-import { CdkTableModule } from '@angular/cdk/table';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-} from '@angular/material';
-
 import { NgModule } from '@angular/core';
+
+import { MaterialModule } from './material.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-@NgModule({
-  exports: [
-    CdkTableModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    BrowserAnimationsModule,
-  ],
-  declarations: []
-})
-export class MaterialModule {}
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FsService } from './fs.service';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EditComponent } from './edit/edit.component';
 import { EditorComponent } from './editor/editor.component';
 import { PlaygroundComponent } from './playground/playground.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { CommandsComponent } from './commands/commands.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -108,7 +34,12 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent, EditComponent, EditorComponent, PlaygroundComponent, PagenotfoundComponent
+    HomeComponent, 
+    EditComponent, 
+    EditorComponent, 
+    CommandsComponent,
+    PlaygroundComponent, 
+    PagenotfoundComponent,
   ],
   imports: [
     MonacoEditorModule.forRoot(),   // use forRoot() in main app module only.
@@ -117,11 +48,11 @@ const routes: Routes = [
       // { enableTracing: true }    // debugging purposes only
     ),
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MaterialModule,
   ],
   providers: [FsService],
   bootstrap: [AppComponent]
