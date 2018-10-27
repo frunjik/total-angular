a4160c88bd19ef7423d58dc78a0e841d9cd4578e
frunjik <frunjik@gmail.com>
2018-10-27 15:05:37 +0200

    separating game / engine - WIP

--- client/src/app/wp3/game.ts
+++ client/src/app/wp3/game.ts
@@ -1,279 +1,134 @@
 import * as THREE from 'three';
-import { CameraControls } from './cameracontrols';
-import { Keyboard, Mouse } from './input';
-import { grassVertexShader, 
-    grassFragmentShader, 
-} from './shaders';
+import { Engine } from './engine';
 
-export class Game  {
-  private w = 640 * 1.5;
-  private h = 480 * 1.5;
-  private raf;
-  private light;
-  private directionalLight;
-  private canvas;
-  private bounds;
-  private cube;
-  private floor;
-  private scene;
-  private camera;
-  private grass;
-  private renderer;
-  private sensitivity;
-  private controls;
-
-  private mouse = new Mouse();
-  private keyboard = new Keyboard();
-  private ticks=0;
-
-  private materials  = {};
-  private geometries = {};
-  private shaders = {};
-
-  private mx = 0;
-  private my = 0;
-  private raycaster;
-
-  getorset(m, name, o) {
-    if (o) {
-        m[name] = o;
+class Piece {
+    x;
+    y;
+    width;
+    height;
+    color;
+    constructor(c, x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.color = c;
+        this.width = w;
+        this.height = h;
     }
-    else {
-        o = m[name];
-    }
-    return o;
-  }
+}
 
-  material(name, m=null) {
-    return this.getorset(this.materials, name, m);
-  }
+class Board {
+    pieces = [];
 
-  geometry(name, g=null) {
-    return this.getorset(this.geometries, name, g);
-  }
-
-  shader(name, s=null) {
-    return this.getorset(this.shaders, name, s);
-  }
-
-  createGrassShader() {
-    const w = 120;
-    const h = 120;
-    return new THREE.ShaderMaterial( {
-        uniforms: {
-            time: { value: (new Date()).getMilliseconds() },
-            resolution: { value: new THREE.Vector2(w, h) }
-        },
-        vertexShader: grassVertexShader,
-        fragmentShader: grassFragmentShader
-    } );
-  }
-
-  create() {
-    this.sensitivity = 0.01;
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, this.w / this.h, 0.1, 1000);
-
-    const away = 15;
-    this.camera.position.x =  0 * away;  
-    this.camera.position.y =  3 * away;  
-    this.camera.position.z =  1 * away;  
-
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    this.geometry('cube', geometry);
-
-    var material = new THREE.MeshLambertMaterial({ color: 0xfa11cc });
-    var floorMaterial = new THREE.MeshBasicMaterial({ color: 0x929385 });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.cube.position.z = 0;  
-    this.cube.position.x = 0;  
-    this.cube.position.y = 5;  
-    this.cube.scale.y = 10;  
-    this.cube.name = 'pole';  
-    this.scene.add(this.cube);
-    this.grass = this.createGrassShader();
-
-    this.directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-    this.scene.add(this.directionalLight);
-
-    this.light = new THREE.AmbientLight( 0x404040 ); // soft white light
-    this.scene.add(this.light);
-
-    this.floor = new THREE.Mesh(geometry, this.grass);
-    this.floor.position.y = -0.5;
-
-    const s = 10;
-    const w = 4;
-    const h = 5;
-
-    this.floor.scale.z = s * h;  
-    this.floor.scale.x = s * w;  
-    this.floor.scale.y = 1;  
-    this.floor.name = 'floor';  
-    this.scene.add(this.floor);
-
-    this.camera.lookAt(this.cube.position);
-
-    this.raycaster = new THREE.Raycaster();
-  }
-
-  init(canvas) {
-    this.canvas = canvas;
-    this.bounds = this.canvas.getBoundingClientRect();
-    this.canvas.width = this.w;
-    this.canvas.height = this.h;
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas } );
-    this.renderer.setSize(this.w, this.h);
-    this.controls = new CameraControls(this.camera, this.canvas);
-    this.start();
-  }
-
-  exit() {
-    this.stop();
-    this.controls = null;
-    this.renderer = null;
-    this.canvas = null;
-  }
-
-  start() {
-    this.raf = requestAnimationFrame((now) => {
-        this.controls.update();
-        this.loop(now);
-    });
-  }
-
-  stop() {
-    if (this.raf) {
-        cancelAnimationFrame(this.raf);
-        this.raf = null;
-    }
-  }
-
-  destroy() {
-      this.exit();
-      while(this.scene.children.length > 0){ 
-        this.scene.remove(this.scene.children[0]);
-      }
-
-      this.cube = null;
-      this.floor = null;
-      this.light = null;
-      this.grass = null;
-      this.directionalLight = null;
-      this.camera = null;
-      this.scene = null;
-
-      this.geometries = null;
-      this.materials = null;
-      this.shaders = null;
-      this.raycaster = null;
- }
-
-  loop(now = 0) {
-    // const s = this.shader('cos');
-    // s.uniforms.time.value = ((now/5000) % 100) / 100;
-    // s.needsUpdate = true;
-
-    // this.raycaster.setFromCamera(this.mouse.position, this.camera);
-    this.raycaster.setFromCamera({x: this.mx, y: this.my}, this.camera);
-	var intersects = this.raycaster.intersectObject(this.scene, true);
-
-    let f = intersects.find(i => i.object.name === 'floor');
-    if (f) {
-        let x = intersects.indexOf(f);
-        if (x>=0) {
-            intersects.splice(x, 1);
-        }
+    constructor() {
+        this.createPieces();
     }
 
-    if (intersects.length) {
-        intersects.forEach(i => console.log(i.object.name));
+    createPieces() {
+        this.createPiece('red',   1, 0, 2, 2);
+        // this.createPiece('green', 0, 0, 1, 2);
+        // this.createPiece('green', 3, 0, 1, 2);
+
+        // this.createPiece('green', 0, 2, 1, 2);
+        // this.createPiece('blue',  1, 2, 2, 1);
+        // this.createPiece('green', 3, 2, 1, 2);
+
+        // this.createPiece('yellow',0, 4, 1, 1);
+        // this.createPiece('yellow',1, 3, 1, 1);
+        // this.createPiece('yellow',2, 3, 1, 1);
+        // this.createPiece('yellow',3, 4, 1, 1);
     }
 
-    this.renderer.render(this.scene, this.camera);
-    this.raf = requestAnimationFrame((now) => {
-        this.loop(now);
-    });
-  }
+    createPiece(c, x, y, w, h) {
+        this.pieces.push(new Piece(c, x, y, w, h));
+    }
+}
 
-  onEvent(event) {
-  }
+export class Game extends Engine {
 
-  onWheelEvent(event) {
-// console.log(event.wheelDelta);
-      if (event.wheelDelta>0) {
-            this.controls.dollyIn(event.wheelDelta * this.sensitivity);
-      }
-      else {
-            this.controls.dollyOut(-event.wheelDelta * this.sensitivity);
-      }
-      this.controls.update();
-  }
+    s = 10;
+    position = new THREE.Vector3(-15, 1, -20);
+    blocks = [];
+    board = new Board();
 
-  onClickEvent(event) {
-  }
+    createShapes() {
+        this.geometry('red', new THREE.BoxGeometry(this.s * 2, 1, this.s * 2));
+        this.geometry('green', new THREE.BoxGeometry(this.s * 1, 1, this.s * 2));
+        this.geometry('blue', new THREE.BoxGeometry(this.s * 2, 1, this.s * 1));
+        this.geometry('yellow', new THREE.BoxGeometry(this.s * 1, 1, this.s * 1));
+    }
 
-  onDblClickEvent(event) {
-  }
-
-  onMouseDownEvent(event) {
-      this.mouse.onMouseDown(event);
-  }
-
-  onMouseUpEvent(event) {
-      this.mouse.onMouseUp(event);
-  }
-
-  onKeyDownEvent(event) {
-      this.keyboard.onKeyDown(event);
-      if(this.keyboard.keys[' ']){
-        this.controls.reset();
-      }
-  }
-
-  onKeyUpEvent(event) {
-      this.keyboard.onKeyUp(event);
-  }
-
-  onMouseMove(event) {
+    createMaterials() {
+        // const m = new THREE.LineBasicMaterial({ 
+        //     color: 0xff0000,
+        //     opacity: 0,
+        //     linewidth: 10,
+        //     scale: 2,
+        //     dashSize: 4,
+        //     gapSize: 4
+        // });
 
 
+// var m = new THREE.LineDashedMaterial( {
+// 	color: 0xffffff,
+// 	linewidth: 1,
+// 	scale: 1,
+// 	dashSize: 3,
+// 	gapSize: 1,
+// } );        
 
-    this.mx = ( (event.clientX-this.bounds.x) / this.canvas.width ) * 2 - 1;
-	this.my = - ( (event.clientY-this.bounds.y) / this.canvas.height ) * 2 + 1;
+// var m = new THREE.MeshBasicMaterial( { wireframe: true } );
+var m = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
 
-// console.log(mx, my);
+console.log(m);
+        // this.material('red', new THREE.MeshLambertMaterial({ color: 0xff0000 }));
+        // this.material('green', new THREE.MeshLambertMaterial({ color: 0x00ff00 }));
+        // this.material('blue', new THREE.MeshLambertMaterial({ color: 0x0000ff }));
+        // this.material('yellow', new THREE.MeshLambertMaterial({ color: 0xffff00 }));
+        this.material('red', m);
+    }
 
-    this.mouse.onMouseMove(event);
-    if(this.mouse.left) {
-        if (this.keyboard.keys['Shift']) {
-            this.controls.rotateLeft(event.movementX * this.sensitivity);
-            this.controls.rotateUp(event.movementY * this.sensitivity);
-        }
-        else {
-            this.controls.pan(event.movementX, event.movementY);
-        }
-        this.controls.update();
-    } 
-    if(this.mouse.right) {
-        if (this.keyboard.keys['Shift']) {
-            if(event.movementY) {
+    createBlockFromPiece(p) {
+        this.blocks.push(
+            this.createBlock(p.x, p.y, p.width, p.height, p.color)
+        );
+    }
 
-                if (event.movementY>0) {
-                        this.controls.dollyOut(120 * this.sensitivity);
-                }
-                else {
-                        this.controls.dollyIn(120 * this.sensitivity);
-                }
-                
-                this.controls.update();
-            }
-        }
-    } 
-  }
+    createBlock(x, y, w, h, color) {
 
-  get keys() {
-    return Object.keys(this.keyboard.keys);
-  }
+        const m = this.material(color);
+        const g = this.geometry(color);
+        // g.computeLineDistances();
+        // console.log('M', m);
+
+        const b = new THREE.Mesh(g, m);
+        b.position.x = this.position.x + (this.s * (x + (w-1)/2));
+        b.position.y = 0.5;
+        b.position.z = this.position.z + (this.s * (y + (h-1)/2));
+        b.name = color;
+
+        return b;
+    }
+
+    createBlocks() {
+        this.createShapes();
+        this.createMaterials();
+        this.board.pieces.forEach(p => this.createBlockFromPiece(p));
+    }
+
+    create() {
+
+console.log('wp3.game');
+
+
+       super.create();
+       this.createBlocks();
+       this.blocks.forEach(b => this.scene.add(b));
+    }
+
+    destroy() {
+       this.blocks.forEach(b => this.scene.remove(b));
+       this.blocks = [];
+       super.destroy();
+    }
 }
  
